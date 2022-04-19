1356c207a9fb039dbc4842dce52fb7f825838450
Ellen White <ellenwhite@macbook-air-4.attlocal.net>
2022-04-19 13:59:40 -0500

    need to fix bindtopopup

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-const jsonData = []
-d3.json("http://127.0.0.1:5000").then(async data => {
-    console.log("read samples");
-    var mydata = JSON.parse(data)
-    // console.log(mydata)
-    // 
-    // list of varieties
-    var varietyList = [];
-    for (var i = 0; i < mydata.length; i++) {
-        varietyList.push(mydata[i].variety)
-    }
-    console.log(varietyList);
+// const jsonData = []
+// d3.json("http://127.0.0.1:5000").then(async data => {
+//     console.log("read samples");
+//     var mydata = JSON.parse(data)
+//     // console.log(mydata)
+//     // 
+//     // list of varieties
+//     var varietyList = [];
+//     for (var i = 0; i < mydata.length; i++) {
+//         varietyList.push(mydata[i].variety)
+//     }
+//     console.log(varietyList);
 
-    // list of provinces
-    var provinceList = [];
-    for (var i = 0; i < mydata.length; i++) {
-        provinceList.push(mydata[i].province)
-    }
-    // list of regions
-    var regionList = [];
-    for (var i = 0; i < mydata.length; i++) {
-        regionList.push(mydata[i].region_1)
-    }
-    // list of wineries
-    var wineryList = [];
-    for (var i = 0; i < mydata.length; i++) {
-        wineryList.push(mydata[i].winery)
-    }
-    // for coordinates
+//     // list of provinces
+//     var provinceList = [];
+//     for (var i = 0; i < mydata.length; i++) {
+//         provinceList.push(mydata[i].province)
+//     }
+//     // list of regions
+//     var regionList = [];
+//     for (var i = 0; i < mydata.length; i++) {
+//         regionList.push(mydata[i].region_1)
+//     }
+//     // list of wineries
+//     var wineryList = [];
+//     for (var i = 0; i < mydata.length; i++) {
+//         wineryList.push(mydata[i].winery)
+//     }
+//     // for coordinates
 
-    var countryProv = wineryList.map(function (e, i,) {
-        return [e, regionList[i]];
-    }).map(([winery, region]) => `${winery}, ${region}`)
+//     var countryProv = wineryList.map(function (e, i,) {
+//         return [e, regionList[i]];
+//     }).map(([winery, region]) => `${winery}, ${region}`)
 
-    var countryProvSet = new Set(countryProv)
-    console.log(countryProvSet);
-    countryProv = [...countryProvSet]
-    console.log(countryProv);
+//     var countryProvSet = new Set(countryProv)
+//     console.log(countryProvSet);
+//     countryProv = [...countryProvSet]
+//     console.log(countryProv);
 
     // for (let i = 0; i < countryProv.length; i++) {
     //     var country = countryProv[i]
@@ -68,14 +68,14 @@ d3.json("http://127.0.0.1:5000").then(async data => {
     //   a.textContent = "coords.json";
     //   a.click()
 
-    d3.json("http://127.0.0.1:5000/coords").then(async data => {
+    d3.json("http://127.0.0.1:5000/").then(async data => {
         console.log("read samples");
         var coorddata = JSON.parse(data)
         console.log(coorddata);
 
         var myMap = L.map("map", {
-            center: [45.52, -122.67],
-            zoom: 3
+            center: [39.0997, -94.5786],
+            zoom: 4.4
         });
 
         // Adding a tile layer (the background map image) to our map:
@@ -84,38 +84,33 @@ d3.json("http://127.0.0.1:5000").then(async data => {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(myMap);
         var markers = []
-        for (var i = 0; i < coorddata.length; i++) {
-        var marker = L.marker([coorddata[i].latitute, coorddata[i].longitude])
+        var lats = []
+        var lngs = []
+        var winery = []
+        var avgPoints = []
+        var avgPrice = []
+       
+        for (const [key, value] of Object.entries(coorddata[0].latitute)) {
+            lats.push(`${value}`);
+            winery.push(`${key}`)}
+            console.log(winery)
+        for (const [key, value] of Object.entries(coorddata[0].longitude)) {
+            lngs.push(`${value}`);}
+        for (const [key, value] of Object.entries(coorddata[0].points)) {
+            avgPoints.push(`${value}`);}
+        for (const [key, value] of Object.entries(coorddata[0].price)) {
+            avgPrice.push(`${value}`);}
+        
+        for (var i = 0; i < 333; i++) {
+    
+        var marker = L.marker([lats[i], lngs[i]]).bindPopup("<h1>" + winery[i].name + "</h1>").openPopup();
+       
         markers.push(marker);
         };
         var newlayerGroup = L.layerGroup(markers)
         newlayerGroup.addTo(myMap)
             
-        })})
+        })
 // merge collection in pandas, export as json to new api page, use above code
 
-    // for (var i = 0; i < countryProv.length; i++) {
-    //     if countryProv[i] 
-
-    //     var wineries = []
-    //     for (var i = 0; i < coorddata.length; i++) {
-        
-    //             var winery = coorddata[i];
-
-    //         // For each station, create a marker, and bind a popup with the station's name.
-    //         try {
-    //             var winerymarker = L.marker([winery.latitude, winery.longitude])
-    //                 .bindPopup("<h3>" + winery.address);
-
-    //         // Add the marker to the bikeMarkers array.
-    //             wineries.push(winerymarker);}
-
-    //         catch { 
-    //             console.log("invalid")
-    //         }}
-        
-    //     // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-    //     var layer = (L.layerGroup(wineries));
-    //     layer.addTo(map)
-    // })
 
