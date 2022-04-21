bae340c61299283bf8b83498de565afac39c7997
SimplyLizzy <elingu94@gmail.com>
2022-04-20 22:01:15 -0500

    html update

--- andrewapp.js
+++ andrewapp.js
@@ -1,45 +1,44 @@
-
 function buildMetadata(sample) {
     
-    // Pull samples.json then call it (data)
-    d3.json("http://127.0.0.1:5000").then(data => {
-      console.log("read samples");
-      var data = JSON.parse(data)
-      
-      
-      // Filter the data for the object with the desired sample number
-      // Go to metadata / filter the following, if id == sample
-      var resultArray = data.filter(x => x.variety == sample);
-    // console.log(resultArray[1].country)
-    var resultCountry = []
-      for(var i = 0; i<resultArray.length; i++) {
-        resultArray[i].country
-        resultCountry.push(resultArray[i].country);
-        }
-    console.log(resultCountry);
-    uniqueCountry = Array.from(new Set(resultCountry));
-    // Sort: Alphabetical Order
-    uniqueCountry.sort();
-    //Print Results
-    console.log(uniqueCountry);
-      
-      // Use d3 to select the panel with id of `#sample-metadata`
-      var PANEL = d3.select("#sample-metadata");
-  
-      // Use `.html("") to clear any existing metadata
-      PANEL.html("");
-  
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // Tags for each key-value in the metadata.
-      Object.entries(uniqueCountry).forEach(([key, value]) => {
-        PANEL.append("h6").text(`${value}`);
-      });
-      // Object.entries(uniqueCountry).forEach(([key, value]) => {
-      //   PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-      // });
+  // Pull samples.json then call it (data)
+  d3.json("http://127.0.0.1:5000").then(data => {
+    console.log("read samples");
+    var data = JSON.parse(data)
+    
+    
+    // Filter the data for the object with the desired sample number
+    // Go to metadata / filter the following, if id == sample
+    var resultArray = data.filter(x => x.variety == sample);
+  // console.log(resultArray[1].country)
+  var resultCountry = []
+    for(var i = 0; i<resultArray.length; i++) {
+      resultArray[i].country
+      resultCountry.push(resultArray[i].country);
+      }
+  console.log(resultCountry);
+  uniqueCountry = Array.from(new Set(resultCountry));
+  // Sort: Alphabetical Order
+  uniqueCountry.sort();
+  //Print Results
+  console.log(uniqueCountry);
+    
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // Tags for each key-value in the metadata.
+    Object.entries(uniqueCountry).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${value}`);
     });
-  }
+    // Object.entries(uniqueCountry).forEach(([key, value]) => {
+    //   PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    // });
+  });
+}
 
 
 
@@ -48,45 +47,42 @@ function buildMetadata(sample) {
 
 
 function init() {
-  d3.json("http://127.0.0.1:5000/variety-list").then(data => {
-      console.log("read samples");
-      console.log("data",data)
+d3.json("http://127.0.0.1:5000").then(data => {
+    console.log("read samples");
+    var data = JSON.parse(data)
+    console.log(data)
 
-    //     // Create a list for wine variety options
-    //     var varietyList = [];
+      // Create a list for wine variety options
+      var varietyList = [];
 
-    //     // Loop through data[0] to data.length
-    //     // Pull all wine variety options
-    //     for(var i = 0; i<data.length; i++) {
-    //         data[i].variety
-    //         varietyList.push(data[i].variety)
-    // }
-    //     console.log(varietyList);
+      // Loop through data[0] to data.length
+      // Pull all wine variety options
+      for(var i = 0; i<data.length; i++) {
+          data[i].variety
+          varietyList.push(data[i].variety)
+  }
+      console.log(varietyList);
 
-    //     // Remove Duplicates
-    //     uniqueVarietyList = Array.from(new Set(varietyList));
-    //     // Sort: Alphabetical Order
-    //     uniqueVarietyList.sort();
-    //     //Print Results
-    //     console.log(uniqueVarietyList);
+      // Remove Duplicates
+      uniqueVarietyList = Array.from(new Set(varietyList));
+      // Sort: Alphabetical Order
+      uniqueVarietyList.sort();
+      //Print Results
+      console.log(uniqueVarietyList);
 
 
-        // Drop down menu creation
-        let dropdownMenu = d3.select("#selDataset");
-        data.forEach((uniqueVarietyList) => {
-            dropdownMenu.append('option').text(uniqueVarietyList)
-            // dropdownMenu.append('option').text(uniqueVarietyList).property("value",UniqueVarietyList);
-        })
-        //this is works because of Closure and Scope (Unique)
-        // Start at wine option 0
-        var result = data[0];
+      // Drop down menu creation
+      let dropdownMenu = d3.select("#selDataset");
+      uniqueVarietyList.forEach((uniqueVarietyList) => {
+          dropdownMenu.append('option').text(uniqueVarietyList);
+      })
 
-        // // for (let i = 0; i<data.length; i++){
-        // // console.log(data.country);
-    buildMetadata(result)
-    //   var result = data[0].variety;
-    // winelist(wineVariety);
-    });
+      // Start at wine option 0
+      var result = uniqueVarietyList[0];
+
+  buildMetadata(result)
+ 
+  });
 }
 
 // New Option for Drop Down, re-load all info
--- app.py
+++ app.py
@@ -14,12 +14,10 @@ CORS(app)
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/project3")
 
-
 @app.route("/")
 def homepage():
     # want country, province, price, and points to be NOT null
     data = mongo.db.wines.find({},{'_id': 0,'taster_name':0,'taster_twitter_handle':0,'designation':0,'region_1':0,'region_2':0}).limit(5000)
-    # data = mongo.db.wines.find({},{variety: 1, _id: 0})
     # data = mongo.db.wines.find({}).limit(5000)
      #this is the array produced on our local host server, can filter instead of select on js page
     list_cur = list(data)
@@ -28,37 +26,6 @@ def homepage():
     # print(type(json))
     json_data = jsonify(json_util.dumps(list_cur))
     return json_data
-
-
-@app.route("/variety-list")
-def variety_list():
-   
-    data = mongo.db.wines.distinct('variety')
-    
-    list_cur = list(data)
-    
-    return jsonify(list_cur)
-
-
-# @app.route("/")
-# def homepage():
-#     data = mongo.db.wines.find() #this is the array produced on our local host server, can filter instead of select on js page
-#     list_cur = list(data)
-#     json_data = jsonify(json_util.dumps(list_cur))
-#     return json_data
-
-@app.route("/select/<variety-list>")
-def select(variety-list):
-    data = mongo.db.wines.find({},{'_id': 0,'taster_name':0,'taster_twitter_handle':0,'designation':0,'region_1':0,'region_2':0})
-    list_cur = list(data)
-#     #print(list_cur)
-#     #json_data = jsonify(list_cur)
-    json_data = jsonify(json_util.dumps(list_cur))
-    return jsonify(list_cur)
-
-#templates or d3.json to call server
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
