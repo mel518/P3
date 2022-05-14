a5bc9270770650eb362fd7a3179a9a051b549e85
Ellen White <ellenwhite@macbook-air-4.attlocal.net>
2022-05-14 15:35:25 -0500

    done

--- nii_charts.js
+++ nii_charts.js
@@ -1,54 +1,10 @@
-
-function init() {
-
-  d3.json("https://egwhitewineapp.herokuapp.com").then(data => {
-    console.log('top data');
-    var data = JSON.parse(data)
-    console.log(data)
-
-    
-  var wineVariety = data[0];
-  console.log(wineVariety);
-
-  // Create a list for wine variety options
-  var varietyList = [];
-
-  // Loop through data[0] to data.length
-  // Pull all wine variety options
-  for(var i = 0; i<data.length; i++) {
-      data[i].variety
-      varietyList.push(data[i].variety)
-}
-  console.log(varietyList);
-
-  // Remove Duplicates
-  uniqueVarietyList = Array.from(new Set(varietyList));
-  // Sort: Alphabetical Order
-  uniqueVarietyList.sort();
-  //Print Results
-  console.log(uniqueVarietyList);
-
-
-  // Drop down menu creation
-  let dropdownMenu = d3.select("#selDataset");
-  // uniqueVarietyList.forEach((uniqueVarietyList) => {
-  //     dropdownMenu.append('option').text(uniqueVarietyList);
-  // })
-
-  // Start at wine option 0
-  var result = uniqueVarietyList[0];
-
-});
-}
-
 function scatterpoints(sample){
-  d3.json("https://egwhitewineapp.herokuapp.com").then(data => {
+  d3.json(`https://egwhitewineapp.herokuapp.com/select/${sample}`).then(data => {
         console.log('top data');
-        var data = JSON.parse(data)
         console.log(data)
     
   
-     var resultArray = data.filter(x => x.variety == sample);
+     var resultArray = data
 
       let winepoints= [];
       let wineprice= [];
@@ -85,12 +41,13 @@ function scatterpoints(sample){
         x: (winepoints),
         y: (wineprice),
         //text: (otu_labelss),
+        text: [winename],
         mode: 'markers',
         marker: {
         color: (winepoints),
-        // size: (5),
-        colorscale: 'Fire',
-        text: [winename]
+        size: (15),
+        colorscale: 'Fire'
+        
       }
     };
     
@@ -144,7 +101,3 @@ function optionChanged(value) {
   scatterpoints(value);
   second_chart(value);
 }
-
-// Initialize the code
-init();
-
