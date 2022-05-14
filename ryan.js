3fd6532629c593b9509c3b7d5a128758116ae182
Ellen White <ellenwhite@macbook-air-4.attlocal.net>
2022-05-14 15:30:41 -0500

    done

--- ryan.js
+++ ryan.js
@@ -1,128 +1,75 @@
-//Display the information based on which wine is selected
 function second_chart(sample) {
-    //read the flask 
-    d3.json("https://egwhitewineapp.herokuapp.com").then(data => {
+    //read the flask
+    d3.json(`https://egwhitewineapp.herokuapp.com/select/${sample}`).then(data => {
         console.log('top data');
-        var data = JSON.parse(data)
         console.log(data)
         // filter the data by variety
-        let results = data.filter(varty => varty.variety === sample);
+        let results = data
         console.log(results)
         //get the top 3 results
-        let top_results = results.slice(0, 3).reverse();
-        console.log(top_results)
-        let length = top_results.length
+        // let top_results = results.slice(0, 3).reverse();
+        // console.log(top_results)
+    //sort and filter to get the wines in descending order based on points
+        let filterMap = {};
+        results.forEach(function (item) {
+            if (!filterMap[item.points] || filterMap[item.points].points < item.points){
+                filterMap[item.points] = item;
+            }
+        })
+        let meta = [];
+        for (let points in filterMap) {
+            meta.push(filterMap[points]);
+        }
+        meta.sort(function(a,b){
+            return b.points - a.points;
+        });
+        console.log(meta);
+        //find the length of selected wine choice
+        let length = meta.length
         console.log(length)
-
-
-
         //display the top option
         if (length > 1) {
-            if (length > 1) {
-                let displayinfo = d3.select('#sample-metadata2');
-                displayinfo.html('');
-                Object.entries(top_results[0]).forEach(k => {
-                    console.log(k)
-                    displayinfo.append('panel-body').text(k[0] + ':' + k[1]).append('br');
-                    displayinfo.append('panel-body').text(k[0].toUpperCase() + ': ' + k[1]).append('br');
-                });
-                //display the second top option
-                let displayinfo2 = d3.select('#sample-metadata3');
-                displayinfo2.html('');
-                Object.entries(top_results[1]).forEach(k2 => {
-                    console.log(k2)
-                    displayinfo2.append('panel-body').text(k2[0] + ':' + k2[1]).append('br');
-                });
-            } else {
-                displayinfo2.append('panel-body').text(k2[0].toUpperCase() + ': ' + k2[1]).append('br');
-            }
+            let displayinfo = d3.select('#sample-metadata2');
+            displayinfo.html('');
+            Object.entries(meta[0]).forEach(k => {
+                console.log(k)
+                displayinfo.append('panel-body').text(k[0].toUpperCase() + ': ' + k[1]).append('br');
+            });
+            //display the second top option
+            let displayinfo2 = d3.select('#sample-metadata3');
+            displayinfo2.html('');
+            Object.entries(meta[1]).forEach(k2 => {
+                console.log(k2)
+                displayinfo2.append('panel-body').text(k2[0].toUpperCase() + ':' + k2[1]).append('br');
+            });
         } else {
             let displayinfo = d3.select('#sample-metadata2');
             displayinfo.html('');
-            Object.entries(top_results[0]).forEach(k => {
+            Object.entries(meta[0]).forEach(k => {
                 console.log(k)
-                displayinfo.append('panel-body').text(k[0] + ':' + k[1]).append('br');
                 displayinfo.append('panel-body').text(k[0].toUpperCase() + ': ' + k[1]).append('br');
             });
-            let displayinfo2 = d3.select('#sample-metadata3');
-            displayinfo2.html('');
-        };
-        //Display the top 10 options
-        let title = data.filter(meta => meta.variety === sample);
-        console.log(title)
-        var top_ten = []
-        for (var i = 0; i < title.length; i++) {
-            title[i].title
-            top_ten.push(title[i].title);
         }
-        console.log(top_ten);
-
-        let top_ten2 = top_ten.slice(0, 10).reverse();
+        //Display the top 10 options
+        console.log(meta)
+        //return just the top options
+        let test = [...meta].slice(0,10);
+        console.log(test)
+        //collect all the wine titles based on the selected wine choice
+        let winename = [];
+        for(var i = 0; i<test.length; i++) {
+            test[i].price
+            winename.push(test[i].title);
+          }
+        console.log(winename)
+        //display the top 10 options
+        number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         let displayinfo3 = d3.select('#sample-metadata4');
         displayinfo3.html('')
-        Object.entries(top_ten2).forEach(([key, value]) => {
-            displayinfo3.append("panel-body").text(`${value}`).append('br');
-            // let results2 = data.filter(varty => varty.variety === sample);
-            // console.log(results2)
-            // //get the top 3 results
-            // let top_results2 = results2.title.slice(0, 10).reverse();
-            // console.log(top_results2)
-            // // let length = top_results.length
-            // // console.log(length)
-            // let displayinfo = d3.select('#sample-metadata4');
-            // displayinfo.html('');
-            // Object.entries(top_results2).forEach(([key, value]) => {
-            //     displayinfo3.append("panel-body").text(`${value}`);
+        Object.entries(winename).forEach(([key, value]) => {
+            console.log(value)
+            displayinfo3.append('panel-body').text(number[key] + `.` + `${value}`).append('br');
         });
+        console.log(meta)
     });
 }
-
-
-function init() {
-    d3.json("https://egwhitewineapp.herokuapp.com").then(data => {
-        console.log('sample data');
-        var data = JSON.parse(data)
-        console.log(data)
-        var wineVariety = data[0];
-        console.log(wineVariety);
-
-        // Create a list for wine variety options
-        var varietyList = [];
-
-        // Loop through data[0] to data.length
-        // Pull all wine variety options
-        for (var i = 0; i < data.length; i++) {
-            data[i].variety
-            varietyList.push(data[i].variety)
-        }
-        console.log(varietyList);
-
-        // Remove Duplicates
-        uniqueVarietyList = Array.from(new Set(varietyList));
-        // Sort: Alphabetical Order
-        uniqueVarietyList.sort();
-        //Print Results
-        console.log(uniqueVarietyList);
-
-
-        // Drop down menu creation
-        let dropdownMenu = d3.select("#selDataset");
-        // uniqueVarietyList.forEach((uniqueVarietyList) => {
-        //     dropdownMenu.append('option').text(uniqueVarietyList);
-        // });
-
-        // Start at wine option 0
-        var result = uniqueVarietyList[0];
-
-        // for (let i = 0; i<data.length; i++){
-        //     console.log(data.country);
-
-        //     var result = data[0].variety;
-        //     winelist(wineVariety);
-        // };
-    });
-}
-
-// Initialize the code
-init();
-
